d0589554309b27f906785c8ef946a09bd41c97cc
aaharbaugh <aaronmharbaugh@gmail.com>
2019-05-20 16:12:42 -0500

    added basic login functionality

--- plants/src/components/App.js
+++ plants/src/components/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import '../css/App.css';
+import Login from './Login'
+import Home from './Home';
+
+import PrivateRoute from './PrivateRoute.js';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/login">Login</Link>
+        <Link to="/protected">Protected Page</Link>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={Home} />
+      </div>
+      
+    </Router>
   );
 }
 
