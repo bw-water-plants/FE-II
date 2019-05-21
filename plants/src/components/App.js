76c9cbfbc6b1ca57a02f66b7877ffca6ff119965
coriagogo <pkcparis@gmail.com>
2019-05-21 16:39:12 -0400

    styles added to login, signup, app

--- plants/src/components/App.js
+++ plants/src/components/App.js
@@ -8,17 +8,77 @@ import Home from './Home';
 import PrivateRoute from './PrivateRoute.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+import plantcacti from '../assets/plant-cacti2.png';
+
+const AppStyles = styled.div`
+
+`
+
+const HeaderContainer = styled.div`
+    font-family: 'Amatic SC', cursive;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100px;
+    border-bottom: 1px solid black;
+    background-image: url(${plantcacti});
+    background-size: cover;
+    opacity: 0.6;
+    width: 100%;
+
+    a {
+      text-decoration: none;
+      color: white;      
+      text-shadow: 0 0 10px #3F7A1A;    
+      
+      
+    } 
+    h1 {
+        font-size: 50px;
+        @media(min-width: 800px) {
+          font-size: 100px;
+        }
+      }
+
+  
+   
+
+
+`
+
+const HeaderNav = styled.div`   
+
+    a {
+        text-decoration: none;
+        padding: 10px;  
+        color: white;
+        font-size: 25px;
+        font-weight: 900;
+
+            
+    }
+`
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Link to="/login">Login</Link>
-        <Link to="/registration">Registration</Link>
-        <Link to="/protected">Protected Page</Link>
+      <AppStyles>
+        <HeaderContainer>
+          <div className="header-title">
+            <Link to="/protected"><h1>Water My Plant</h1></Link>
+          </div>
+          <HeaderNav>
+            <Link to="/login">Log In</Link>
+            <Link to="/registration">Sign Up</Link>            
+            </HeaderNav>
+        </HeaderContainer>
         <Route path="/login" component={Login} />
         <Route path="/registration" component={Registration} />
         <PrivateRoute exact path="/protected" component={Home} />
-      </div>
+
+        
+        </AppStyles>
       
     </Router>
   );
--- plants/src/components/Login.js
+++ plants/src/components/Login.js
@@ -2,6 +2,44 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions/actions';
 import Loader from "react-loader-spinner";
+import styled from 'styled-components';
+
+const LoginStyles = styled.div`
+    width: 275px;
+    height: 250px;
+    background-color: whitesmoke;
+    margin: 50px auto;
+    border-radius: 15px;
+`
+
+const LoginForm = styled.form`
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    margin: 0 auto;
+
+    input {
+        margin: 10px;
+        height: 35px;        
+        border: none;
+        border-radius: 20px;
+        padding-top: 5px;
+        
+
+        :first-child {
+            margin-top: 25px;
+        }
+
+        ::placeholder {
+            font-size: 25px;
+            text-align: center; 
+            color: hsla(149, 35%, 45%, 1);       
+        }
+
+    }
+
+`
 
 class Login extends React.Component {
 
@@ -31,27 +69,29 @@ class Login extends React.Component {
 
     render(){
         return (
-            <div>
-                <form onSubmit={this.login}>
+            <LoginStyles>
+                <LoginForm onSubmit={this.login}>
                     <input 
                         type="text"
                         name="username"
+                        placeholder="Username"
                         value={this.state.credentials.username}
                         onChange={this.handleChange}
                     />
                     <input 
                         type="text"
                         name="password"
+                        placeholder="Password"
                         value={this.state.credentials.password}
                         onChange={this.handleChange}
                     />
                     <button>
                     {this.props.isLoggingIn 
                             ? <Loader type="ThreeDots" color="#00BFFF" height="25" width="50" /> 
-                            : "Login"}
+                            : "Log In"}
                     </button>
-                </form>
-            </div>
+                </LoginForm>
+            </LoginStyles>
         )
     }
 
