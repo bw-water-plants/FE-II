1b409d68dfddf3fb990c7f0c1c86c4542a8d9d8d
aaharbaugh <aaronmharbaugh@gmail.com>
2019-05-22 14:29:46 -0500

    added ability to add a plant on plant list page

--- plants/src/components/PlantsList.js
+++ plants/src/components/PlantsList.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getPlants } from '../actions/actions';
+import { getPlants, createPlant } from '../actions/actions';
 import Loader from "react-loader-spinner";
 import styled from 'styled-components';
 
 class PlantsList extends React.Component {
 
-    componentDidMount() {
-       this.props.getPlants()
+    state = {
+        isAddingPlant: false,
+        newPlant: {
+            plantName: '',
+            dailyWaterTime: '',
+            user_id: localStorage.getItem('id')
+        }
     }
 
+    componentDidMount() {
+       this.props.getPlants()
+
+    }
+
+    handleChange = e => {
+        this.setState({
+            newPlant: {
+            ...this.state.newPlant,
+            [e.target.name]: e.target.value
+          }
+        });
+      };
 
 
+    addPlant() {
+        this.props.createPlant(this.state.newPlant)
+    }
+    
+    toggleAddPlantForm() {
+        this.setState({
+            ...this.state,
+            isAddingPlant: !this.state.isAddingPlant
+        });
+    };
 
     render() {
-        console.log(this.props.plants)
         const userId = localStorage.getItem('id');
         const myPlants = this.props.plants.filter(plant => (
             plant.user_id = userId
@@ -28,6 +55,29 @@ class PlantsList extends React.Component {
                         {plant.dailyWaterTime}
                     </div>
                 ) )}
+
+                {this.state.isAddingPlant ? 
+                    
+                    <div>                    
+                        <form>
+                            <input
+                                type="text"
+                                name="plantName"
+                                value={this.state.newPlant.plantName}
+                                onChange={this.handleChange}
+                                /><br />
+                            <input
+                                type="text"
+                                name="dailyWaterTime"
+                                value={this.state.newPlant.dailyWaterTime}
+                                onChange={this.handleChange}
+                                /><br />
+                            <button onClick={() => this.addPlant()}>Submit</ button>
+                        </form>
+                    </div>
+
+                : <button onClick={() => this.toggleAddPlantForm()}>Add Plant</button>}
+                
             </div>
         )
     }
@@ -38,4 +88,4 @@ const mapStateToProps = state => ({
 })
 
 
-export default connect(mapStateToProps, { getPlants })(PlantsList)
+export default connect(mapStateToProps, { getPlants, createPlant })(PlantsList)
